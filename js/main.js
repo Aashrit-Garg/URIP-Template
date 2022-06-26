92e12a1edf38ae82100e922ab1ba34b8af076d85
aashritgarg <aashritgarg@gmail.com>
2022-06-26 13:34:07 +0530

    Complete create.html functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 /* Moralis init code */
 fetch('./js/env.json')
     .then(response => response.json())
-    .then(data => {
+    .then(async data => {
         const serverUrl = data.env.MORALIS_SERVER_URL;
         const appId = data.env.MORALIS_APPLICATION_ID;
         Moralis.start({ serverUrl, appId });
+        let license = "All Rights Reserved";
+
+        let query = new Moralis.Query('Tokens');
+        let subscription = await query.subscribe();
+        console.log('subscribed');
+        subscription.on('create', (object) => {
+            console.log('object created');
+            console.log(object);
+        });
 
         /* Authentication code */
         async function login() {
@@ -32,17 +41,50 @@ fetch('./js/env.json')
             document.getElementById("wallet-address").innerHTML = "Wallet Address"
         }
 
+        async function nd() {
+            license = "All Rights Reserved";
+        }
+
+        async function nc() {
+            license = "Some Rights Reserved";
+        }
+
+        async function by() {
+            license = "All Rights Relinquished";
+        }
+
         async function upload() {
-            console.log("Hello");
             const fileInput = document.getElementById("file-upload");
+            const name = document.getElementById("item-name").value;
+            const externalLink = document.getElementById("item-external-link").value;
+            const description = document.getElementById("item-description").value;
+            const unlockableContent = document.getElementById("unlockable-content").checked;
+            const sensitiveContent = document.getElementById("sensitive-content").checked;
+            const confirmAuthority = document.getElementById("confirm-authority").checked;
+            const termsConditions = document.getElementById("terms-conditions").checked;
             // Save file input to IPFS
             const data = fileInput.files[0];
             const file = new Moralis.File(data.name, data);
             await file.saveIPFS();
 
-            console.log(file.ipfs(), file.hash())
+            const metadata = {
+                "name": name,
+                "externalLink": externalLink,
+                "description": description,
+                "unlockableContent": unlockableContent,
+                "sensitiveContent": sensitiveContent,
+                "confirmAuthority": confirmAuthority,
+                "termsAndConditions": termsConditions,
+                "tokenURI": file.ipfs()
+            }
+
+            const nftFile = new Moralis.File("file.json", { base64: btoa(JSON.stringify(metadata)) });
+            await nftFile.saveIPFS();
+
+            console.log(nftFile.ipfs());
+
             let options = {
-                contractAddress: "0xbbC53e183eDdBA39d723E66074CcE518c606Bfe6",
+                contractAddress: "0xa550983721b016611112edf9f24AEce3765683e0",
                 functionName: "createToken",
                 abi: [
                     {
@@ -424,16 +466,34 @@ fetch('./js/env.json')
                     }
                 ],
                 params: {
-                    tokenURI: file.ipfs()
+                    tokenURI: nftFile.ipfs()
                 },
                 msgValue: Moralis.Units.ETH(0.0)
             }
-            let result = await Moralis.executeFunction(options);
-            console.log(result);
+            await Moralis.executeFunction(options).then((result) => {
+                console.log(result);
+                // Save file reference to Moralis
+                const mints = new Moralis.Object("Mints");
+                mints.set("name", "URIP Token");
+                mints.set("ipfs", nftFile);
+                mints.set("urip", license);
+                mints.set("transaction", result.hash);
+                mints.save().then(
+                    (mint) => {
+                        console.log(mint.id);
+                    },
+                    (error) => {
+                        console.log(error);
+                    },
+                );
+            });
         }
 
         document.getElementById("btn-login").onclick = login;
         document.getElementById("btn-logout").onclick = logOut;
         document.getElementById("btn-upload").onclick = upload;
+        document.getElementById("btn-nd").onclick = nd;
+        document.getElementById("btn-nc").onclick = nc;
+        document.getElementById("btn-by").onclick = by;
     })
     .catch(error => console.log(error));
